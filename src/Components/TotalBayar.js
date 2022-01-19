f89e9fa8fe392b2346ede728208d9e119e76a255
Yusup1907 <yusupsetiawan1907@gmail.com>
2022-01-19 08:25:23 +0700

    commit pertama

---
+++ src/Components/TotalBayar.js
@@ -0,0 +1,68 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import React, { Component } from 'react'
+import { Col, Row, Button } from 'react-bootstrap';
+import { numberWithCommas } from '../util/utils';
+import { API_URL } from '../util/constants'
+
+
+export default class ToralBayar extends Component {
+    submitTotalBayar = (totalBayar) => {
+        const pesanan = {
+            total_bayar: totalBayar,
+            menus: this.props.keranjangs
+        }
+        axios.post(API_URL+"pesanans", pesanan).then((res) => {
+            this.props.history.push('/sukses')
+        })
+    };  
+    render() {
+        const TotalBayar = this.props.keranjangs.reduce(function (result, item) {
+            return result + item.total_harga;
+        }, 0);
+        return (
+            <>
+            {/* Web */}
+            <div className="fixed-bottom d-none d-md-block">
+                <Row>
+                    <Col md={{ span: 3, offset: 9 }} className="px-4">
+                        <h4>
+                            Total Harga : {" "}
+                            <strong className="float-right mr-4">
+                                Rp. {numberWithCommas(TotalBayar)}
+
+                            </strong>
+                        </h4>
+                        <Button variant="primary" block className="mb-2 mt-4 mr-2" size="lg"
+                    onClick={ () => this.submitTotalBayar(TotalBayar)}
+                        >
+                            <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+
+            {/* Mobile */}
+            <div className="d-sm-block d-md-none">
+                <Row>
+                    <Col md={{ span: 3, offset: 9 }} className="px-4">
+                        <h4>
+                            Total Harga : {" "}
+                            <strong className="float-right mr-4">
+                                Rp. {numberWithCommas(TotalBayar)}
+
+                            </strong>
+                        </h4>
+                        <Button variant="primary" block className="mb-2 mt-4 mr-2" size="lg"
+                    onClick={ () => this.submitTotalBayar(TotalBayar)}
+                        >
+                            <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+            </>
+        );
+    }
+}
